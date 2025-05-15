27788a22f021d666818df81cac0ac991e9574151
tytyu073iop <fpm.biryuk@bsu.by>
2025-05-15 12:35:44 +0300

    first commit

---
+++ reciver.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <string>
+#include <format>
+
+std::wstring string_to_wstring(const std::string& str) {
+	if (str.empty()) return L"";
+	int size_needed = MultiByteToWideChar(
+		CP_ACP,                  // Используем системную кодовую страницу
+		0,
+		str.c_str(),
+		(int)str.size(),
+		NULL,
+		0
+	);
+	std::wstring wstr(size_needed, 0);
+	MultiByteToWideChar(
+		CP_ACP,
+		0,
+		str.c_str(),
+		(int)str.size(),
+		&wstr[0],
+		size_needed
+	);
+	return wstr;
+}
+
+int main() {
+	std::cout << "Enter name of the bin file: ";
+	std::string name;
+	std::cin >> name;
+
+	name += ".bin";
+	std::wstring wname = string_to_wstring(name);
+
+	std::ofstream MyFile(name);
+	if (!MyFile.is_open()) {
+		std::cerr << "Error opening file!\n";
+		return 1;
+	}
+	MyFile.close();
+
+	size_t amountOfSenders;
+	std::cout << "How much senders: ";
+	std::cin >> amountOfSenders;
+
+	HANDLE* events = new HANDLE[amountOfSenders];
+	PROCESS_INFORMATION* processes = new PROCESS_INFORMATION[amountOfSenders];
+    STARTUPINFO* sis = new STARTUPINFO[amountOfSenders];
+
+	for (size_t i = 0; i < amountOfSenders; i++)
+	{
+        ZeroMemory(&sis[i], sizeof(STARTUPINFO));
+        sis[i].cb = sizeof(STARTUPINFO);
+		LPSTR program = _strdup(("Sender.exe " + name + " " + std::to_string(i)).c_str());
+		if (program == NULL) {
+			std::cout << "ERROR cannot create process name string\n";
+			exit(1);
+		}
+		auto h = CreateProcess(NULL, program, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &sis[i], &processes[i]);
+		if (h == FALSE) {
+			std::cout << "ERRor creating process: " << GetLastError() << '\n';
+		}
+		auto q = CreateEvent(NULL, FALSE, FALSE, ("SenderReady" + std::to_string(i)).c_str());
+		if (q == NULL) {
+			std::cout << "ERror creating event: " << GetLastError() << '\n';
+		}
+		events[i] = q;
+	}
+
+	WaitForMultipleObjects(amountOfSenders, events, TRUE, INFINITE);
+	std::ifstream file(name);
+	if (!file.is_open()) {
+		std::cout << "Error opening file\n";
+		exit(0);
+	}
+
+	while (true) {
+		std::string whatToDo;
+		std::cout << "Read the message or exit application(r/e): ";
+		std::cin >> whatToDo;
+		if (whatToDo == "e") {
+			break;
+		}
+		else if (whatToDo == "r") {
+			file.clear();
+			//	file.seekg(file.tellg());  // Stay at current position
+			std::string line;
+			while (std::getline(file, line)) 
+			{
+				std::cout << "message: " << line << '\n';
+			}
+		}
+		else {
+			std::cout << "wrong message. Repeating.\n";
+		}
+	}
+
+	// do process exiting
+	for (size_t i = 0; i < amountOfSenders; i++)
+	{
+		// there should be cleanup
+	}
+	
+}
---
+++ sender.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <string>
+#include <format>
+
+int one() {
+    std::cout << "Press any key to exit...";
+    std::cin.get();
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cout << "wrong start. Need {filename} {number}";
+        return one();
+    }
+
+    std::string name = argv[1];
+    std::ofstream file(name, std::ios::ate);
+    if (!file) {
+        std::cout << "Eror opening file\n";
+        return one();
+    }
+
+    std::string order = argv[2];
+    std::string eventName = "SenderReady";
+
+    HANDLE event = OpenEvent(EVENT_MODIFY_STATE, FALSE, (eventName + order).c_str());
+    if (event == NULL) {
+        std::cout << "Error creating event: " << GetLastError() << '\n';
+        return one();
+    }
+
+    SetEvent(event);
+
+    while (true)
+    {
+        std::string whatToDo;
+        std::cout << "Send message or exit(s/e)";
+        std::cin >> whatToDo;
+        if (whatToDo == "e") {
+            return 0;
+        }
+        else if (whatToDo == "s") {
+            std::cout << "enter message: ";
+            std::string message;
+            std::cin >> message;
+            file.seekp(0, std::ios::end);
+            file << message << '\n';
+            file.flush();
+            
+        }
+        else {
+            std::cout << "wrong request. Repeating.\n";
+        }
+    }
+    
+}
